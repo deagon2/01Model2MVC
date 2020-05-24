fc9444ce6a45a4cf05faa2ccf00b0dc830e2d156
Daegon <Daegon@LAPTOP-5VDOCNOC>
2020-05-24 09:01:29 +0900

    ??

--- 01.Model2MVCShop(stu)/src/com/model2/mvc/view/purchase/GetPurchaseAction.java
+++ 01.Model2MVCShop(stu)/src/com/model2/mvc/view/purchase/GetPurchaseAction.java
@@ -7,19 +7,19 @@ import com.model2.mvc.framework.Action;
 import com.model2.mvc.service.purchase.PurchaseService;
 import com.model2.mvc.service.purchase.impl.PurchaseServiceImpl;
 import com.model2.mvc.service.purchase.vo.PurchaseVO;
-
+		
 public class GetPurchaseAction extends Action {
 
-		public String execute(	HttpServletRequest request,
-													HttpServletResponse response) throws Exception {
-			PurchaseService service = new PurchaseServiceImpl();
-				
-			PurchaseVO vo = service.getPurchase(Integer.parseInt(request.getParameter("tranNo")));
-			
-			request.setAttribute("vo", vo);
-			
-				return "forward:/purchase/getPurchase.jsp";
-			
+public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		int tranNo=Integer.parseInt(request.getParameter("tranNo"));
+		
+		PurchaseService purchaseService=new PurchaseServiceImpl();
+		PurchaseVO purchase=purchaseService.getPurchase(tranNo);
+		
+		request.setAttribute("purchase", purchase);
+		
+		return "forward:/purchase/getPurchase.jsp";
 	}
 
 }
